00af3444c13e2af7206ce3a406c733e9b3f58637
uday-doppalapudi-ic <udoppala@ic.ac.uk>
2025-06-16 13:15:12 +0530

    Second commit

--- resource-groups.tf
+++ resource-groups.tf
@@ -1,7 +1,7 @@
 # app.terraform.io/<Organization>/<module-name>/<provider>
-module "resoruce-group" {
-  source          = "app.terraform.io/FP-Training/resoruce-group/azurerm"
-  version         = "0.0.1"
-  resource_groups = local.resource_groups
-  tags            = local.tags
+resource "azurerm_resource_group" "resoruce-group" {
+  for_each = local.resource_groups
+  name     = each.value.name
+  location = each.value.location != null ? each.value.location : "UK South"
+  tags     = local.tags
 }
